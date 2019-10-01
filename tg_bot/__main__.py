01e48ae7eb52917d92abc2ad0846ba8860b53832
hyperterminal-byte <sharath3589@yahoo.in>
2019-10-01 09:13:27 +0530

    Customise few strings

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -19,19 +19,14 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-Hi {}, my name is {}! If you have any questions on how to use me, read /help - and then head to @MarieSupport.
+Hi {}, my name is {}! If you have any questions on how to use me, read /help - and then head to [HyperOT](https://t.me/rovaofftopic)
 
-I'm a group manager bot built in python3, using the python-telegram-bot library, and am fully opensource; \
-you can find what makes me tick [here](github.com/PaulSonOfLars/tgbot)!
+I'm a group manager bot completely based on Paul's popular Marie bot built in python3, using the python-telegram-bot library with not many changes.
 
-Feel free to submit pull requests on github, or to contact my support group, @MarieSupport, with any bugs, questions \
-or feature requests you might have :)
-I also have a news channel, @MarieNews for announcements on new features, downtime, etc.
+My master: @hyperterminal
+My source: [Github](https://github.com/hyperterminal-byte/ParallaxBot/)
 
 You can find the list of available commands with /help.
-
-If you're enjoying using me, and/or would like to help me survive in the wild, hit /donate to help fund/upgrade my VPS!
-"""
 
 HELP_STRINGS = """
 Hey there! My name is *{}*.
@@ -51,11 +46,8 @@ the things I can help you with.
 And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
-DONATE_STRING = """Heya, glad to hear you want to donate!
-It took lots of work for my creator to get me to where I am now, and every donation helps \
-motivate him to make me even better. All the donation money will go to a better VPS to host me, and/or beer \
-(see his bio!). He's just a poor student, so every little helps!
-There are two ways of paying him; [PayPal](paypal.me/PaulSonOfLars), or [Monzo](monzo.me/paulnionvestergaardlarsen)."""
+DONATE_STRING = """Heya, really glad to hear you want to donate!
+You can ping my master for that."""
 
 IMPORTED = {}
 MIGRATEABLE = []
